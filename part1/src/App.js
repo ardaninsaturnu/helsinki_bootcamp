97ae64434caaec96f78c36a3766a71b9568dae40
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-04-12 17:52:00 +0300

    all objects concat in a array and mapped into component render. :tada:

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,31 +1,36 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  
+  const parts = [ part1, part2, part3 ]
   
   const Header = (props) => {
     return <h1>{ props.title }</h1>
   }
   
+  console.log( parts )
   const Content = () => (
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      {
+        parts.map(({ name, exercises }, index ) => {
+          return <p key={index}>{name} : { exercises }</p>
+        })
+      }
     </>
   );
   
-  const Total = () => <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  const Total = () => <p>Number of exercises { parts.reduce( (sum, item) => sum + item.exercises, 0 )  }</p>
   
   return (
     <div>
